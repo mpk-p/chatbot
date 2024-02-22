6acb522be7c09ee4858a06bc9d8384b7e356f861
mpk-p <158258514+mpk-p@users.noreply.github.com>
2024-02-22 22:30:15 +0100

    Update games.py

--- games.py
+++ games.py
@@ -8,3 +8,25 @@ Ce fichier gère le mécanisme des mini-jeux.
 class RockPaperScissors:
     def __init__(self) -> None:
         pass
+def RockPaperScissors1 (move_played) :
+    possible_moves = ['Scissors','Rock','Paper']
+    assert move_played in possible_moves
+    ai_move_played = choice(possible_moves)
+    if ai_move_played == move_played : return 'Egalité'
+    else :
+        if move_played == 'Scissors' and ai_move_played == 'Paper' : return 'Victoire'
+        elif move_played == 'Rock' and ai_move_played == 'Scissors' : return 'Victoire'
+        elif move_played == 'Paper' and ai_move_played == 'Rock' : return 'Victoire'
+        else : return 'Défaite'
+
+def RockPaperScissors2 () :
+    possible_moves = ['Scissors','Rock','Paper']
+    ai_move_played = choice(possible_moves)
+    move_played = str(input('Quel coup voulez vous jouez ?'))
+    assert move_played in possible_moves
+    if ai_move_played == move_played : return 'Egalité'
+    else :
+        if move_played == 'Scissors' and ai_move_played == 'Paper' : return 'Victoire'
+        elif move_played == 'Rock' and ai_move_played == 'Scissors' : return 'Victoire'
+        elif move_played == 'Paper' and ai_move_played == 'Rock' : return 'Victoire'
+        else : return 'Défaite'
